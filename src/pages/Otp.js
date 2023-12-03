03dd92c846277957bd8d66feb80b1a77ff7ee537
Suraj kumar <devilsuraj9641@gmail.com>
2023-12-03 23:58:52 +0530

    2nd

--- src/pages/Otp.js
+++ src/pages/Otp.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
-import { ToastContainer, toast } from 'react-toastify';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { userVerify } from "../services/Apis"
+import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import { useLocation, useNavigate } from "react-router-dom";
+import { userVerify } from "../services/Apis";
 
 const Otp = () => {
   const [otp, setOtp] = useState("");
@@ -14,28 +14,29 @@ const Otp = () => {
     e.preventDefault();
 
     if (otp === "") {
-      toast.error("Enter Your Otp")
+      toast.error("Enter Your Otp");
     } else if (!/[^a-zA-Z]/.test(otp)) {
-      toast.error("Enter Valid Otp")
+      toast.error("Enter Valid Otp");
     } else if (otp.length < 6) {
-      toast.error("Otp Length minimum 6 digit")
+      toast.error("Otp Length minimum 6 digit");
     } else {
       const data = {
-        otp, email: location.state
-      }
+        otp,
+        email: location.state,
+      };
 
       const response = await userVerify(data);
       if (response.status === 200) {
         localStorage.setItem("userdbtoken", response.data.userToken);
         toast.success(response.data.message);
         setTimeout(() => {
-          navigate("/dashboard")
-        }, 5000)
+          navigate("/Dashboard");
+        }, 5000);
       } else {
-        toast.error(response.response.data.error)
+        toast.error(response.response.data.error);
       }
     }
-  }
+  };
 
   return (
     <>
@@ -47,15 +48,23 @@ const Otp = () => {
           <form>
             <div className="form_input">
               <label htmlFor="otp">OTP</label>
-              <input type="text" name="otp" id="" onChange={(e) => setOtp(e.target.value)} placeholder='Enter Your OTP' />
+              <input
+                type="text"
+                name="otp"
+                id=""
+                onChange={(e) => setOtp(e.target.value)}
+                placeholder="Enter Your OTP"
+              />
             </div>
-            <button className='btn'  onClick={LoginUser}>Submit</button>
+            <button className="btn" onClick={LoginUser}>
+              Submit
+            </button>
           </form>
         </div>
         <ToastContainer />
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Otp
+export default Otp;
